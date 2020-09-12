4adab83a89c164c07c6450c6a0283ddaf4b87a07
MinSeugn <66229776+MinSeugn@users.noreply.github.com>
2020-09-12 13:39:50 +0900

    Add files via upload

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -14,11 +14,7 @@ void setup() {
 
 void loop() {
   Serial.println(++count);
-  toggle = toggle_state(count); // toggle LED value.
-  digitalWrite(PIN_LED, toggle); // update LED status.
-  delay(1000); // wait for 1,000 milliseconds
-  Serial.println(--count);
-  toggle = toggle_state(count); // toggle LED value.
+  toggle = toggle_state(count % 2); // toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); // wait for 1,000 milliseconds
 }
